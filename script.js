6798f32ca103d96f3e330cb81b653d5aee75d21c
bwagner882 <78119570+bwagner882@users.noreply.github.com>
2021-03-31 17:46:51 -0500

    adding background color work

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ const clicked = () =>{
     let element = document.getElementById('head');
     console.log(number);
     element.innerHTML = 'I LOVE YOU SO MUCH';
+    let backgroundElements = document.getElementsByClassName('background');
+    backgroundElements.array.forEach(element => {
+        element.style.backgroundColor = 'red';
+    });
 }
