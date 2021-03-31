bca60a2d95dffcb18cafb9783e6051df8a909337
bwagner882 <78119570+bwagner882@users.noreply.github.com>
2021-03-31 18:46:35 -0500

    test

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const number = 5;
+let backgroundElements = document.getElementsByClassName('background');
+let colors = ['red','blue','green','purple','pink','orange','yellow','violet','teal','white'];
+
+let ele = document.getElementById('head');
+ele.innerHTML = "What's up Paige??";
 const clicked = () =>{
     let element = document.getElementById('head');
     console.log(number);
     element.innerHTML = 'I LOVE YOU SO MUCH';
-    let backgroundElements = document.getElementsByClassName('background');
-    backgroundElements.forEach(element => {
-        element.style.backgroundColor = 'red';
-    });
+    for(let i = 0; i < 200; i++){
+        let color = colors[Math.floor(Math.random()*colors.length)];
+        for (let index = 0; index < backgroundElements.length; index++) {
+            const element = backgroundElements[index];
+            element.style.backgroundColor = color;
+            console.log(index, i);
+        }
+    }
+    document.getElementById('button').innerHTML = 'CLICK ME MORE!!!';
 }
